2e484c4443ed65bb0a214c133538a180838ee370
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-11-27 21:03:01 +0000

    Update Euler_13.py

--- Euler_13/Euler_13.py
+++ Euler_13/Euler_13.py
@@ -4,4 +4,13 @@ with open('Euler_13.txt') as file:
 
 lines = list(map(lambda s: s.strip(), lines))
 
-print(lines)
+intLines = list(map(int, lines))
+
+print(intLines)
+
+for x in intLines:
+
+    Array = [x]
+
+
+print(Array)
