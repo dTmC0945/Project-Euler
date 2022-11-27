b917a92ec03d7ef927fafb7074b45a4208700108
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-11-27 21:17:23 +0000

    Update Euler_13.py

--- Euler_13/Euler_13.py
+++ Euler_13/Euler_13.py
@@ -1,13 +1,19 @@
-
+# open the textfile
 with open('Euler_13.txt') as file:
     lines = file.readlines()
 
+# remove the \n from each line
 mappedLines = list(map(lambda s: s.strip(), lines))
 
+# get the first eleven digits of each number
 firstTen = [x[0:11] for x in mappedLines]
 
+# convert the string values to integers
 intLines = list(map(int, firstTen))
 
+# output the sum of the values
 output = str(sum([x for x in intLines]))
 
+# print out the ten digits
 tendigits = print(output[0:10])
+
