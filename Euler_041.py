4856dfe29e91c96e32cb0ca610e510047139958e
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-11-29 08:52:34 +0000

    Update Euler_041.py

--- Euler_041.py
+++ Euler_041.py
@@ -5,11 +5,28 @@ start_time = time.time()
 
 
 def primeFinder(n):
-    for div in range(2, int(np.sqrt(n))):
-        if n % div == 0 and n != 0:
-            break
-        else:
-            return n
+    for div in range(2, int(np.sqrt(n)) + 1):
+        if n % div == 0 and n != div:
+            return 0
 
+    return n
+
+
+looper = 999999999
+
+while looper > 100000000:
+    prime = primeFinder(looper)
+    if prime != 0:
+        intArray = [char for char in str(prime)]
+        if len(intArray) == len(set(intArray)):
+            print(prime)
+            looper -= 2
+        else:
+            looper -= 2
+            continue
+
+    else:
+        looper -= 2
+        continue
 
 print("--- %s seconds ---" % (time.time() - start_time))
