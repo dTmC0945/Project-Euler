e33a6d8ee52a78bbd9e73c25d0c0193c0241d234
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-11-29 18:29:02 +0000

    Update Euler_022.py

--- Euler_022/Euler_022.py
+++ Euler_022/Euler_022.py
@@ -9,16 +9,18 @@ with open('Euler_022.txt') as file:
 # separate each name with comma (,)
 names = sorted(lines[0].split(','))
 
+# initialise the sum
 total = 0
 
+# look in evey name in the list
 for loc in range(0, len(names)):
     person = list(names[loc])
     name_array = []
-
+    # look in every character of a name
     for index in range(1, len(names[loc]) - 1):
         value = ord(person[index]) - 64
         name_array.append(value)
-
+    # sum them up
     output = sum([x for x in name_array]) * (loc + 1)
     total = output + total
 
