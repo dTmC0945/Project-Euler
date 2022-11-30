b9903d6f0e859e84258ae6ecd7b78938be9fd2f8
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-11-30 21:19:32 +0000

    Create Euler_001.py

---
+++ Python/Euler_001.py
@@ -0,0 +1,19 @@
+# Project Euler Question 1 - Multiples of 3 or 5
+
+import time  # import for time calculation
+
+start_time = time.time()  # start the clock
+
+# Average solve time (M1 Mac) - 0.0001 sec
+
+total = 0
+
+for index in range(1, 1000):
+    if index % 3 == 0 or index % 5 == 0:
+        total = index + total
+
+print("The sum of all the multiples of 3 or 5 below 1000 is:", total)
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+# End of code
