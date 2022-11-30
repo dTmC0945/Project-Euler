87b115375450335c24bcf20b06e50e49c0f3c6be
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-11-30 14:49:00 +0000

    Update Euler_065.py

--- Euler_065.py
+++ Euler_065.py
@@ -12,28 +12,19 @@ def fractionalSum(number, array):
     if number == 0:
         return 0
     elif number == 1:
-        return inside(1, 0)
+        return inside(array[0], 0)
     #elif number == 2:
     #    return inside(1, inside(array[2], 0))
-   # elif number == 3:
-      #  return inside(1, inside(array[2], inside(array[3], 0)))
+    #elif number == 88:
+       # return inside(array[1], inside(array[2], inside(array[3], inside(array[4], inside(array[5], 0)))))
     else:
-        cont = number - 2
-        ind = 1
-        while cont >= 0:
-            if cont > 0:
-                cont -= 1
-
-            place = inside(array[ind + 1], cont)
-            output = inside(1, place)
-            ind += 1
-            return output
+        return inside(array[number-2], inside(array[number-1], 0))
 
 
 
 # Fraction(1, array[1] + Fraction(1, array[2] + Fraction(1, array[3])))
 
-expansion = [2, 1]
+expansion = [1]
 it = 1
 clock = 0
 for i in range(1, 110):
@@ -45,4 +36,4 @@ for i in range(1, 110):
         expansion.append(1)
         clock -= 1
 print(expansion)
-print(expansion[0] + fractionalSum(4, expansion))
+print(2 + fractionalSum(4, expansion))
