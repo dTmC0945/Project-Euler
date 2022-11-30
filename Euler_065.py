bc5d1a9ed88723743fe2fa76dd1fa4cfe3613469
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-11-30 15:11:32 +0000

    Update Euler_065.py

--- Euler_065.py
+++ Euler_065.py
@@ -1,28 +1,24 @@
 from fractions import Fraction
 
 
-def inside(index, place):
-    if place >= 0:
-        return Fraction(1, index + place)
-    else:
-        return Fraction(1, index)
-
-
 def fractionalSum(number, array):
+    def inside(index, place):
+        if place >= 0:
+            return Fraction(1, index + place)
+        else:
+            return Fraction(1, index)
+
     if number == 0:
         return 0
     elif number == 1:
-        return inside(array[0], 0)
-    #elif number == 2:
-    #    return inside(1, inside(array[2], 0))
-    #elif number == 88:
-       # return inside(array[1], inside(array[2], inside(array[3], inside(array[4], inside(array[5], 0)))))
+        return inside(array[number - 1], 0)
+    elif number == 2:
+        return inside(1, fractionalSum(number - 1, array))
+    elif number == 88:
+        return inside(array[0], inside(array[1], inside(array[2], inside(array[3], inside(array[4], 0)))))
     else:
-        return inside(array[number-2], inside(array[number-1], 0))
+        return inside(fractionalSum(number - 2, array), inside(fractionalSum(number - 1, array), 0))
 
-
-
-# Fraction(1, array[1] + Fraction(1, array[2] + Fraction(1, array[3])))
 
 expansion = [1]
 it = 1
@@ -36,4 +32,4 @@ for i in range(1, 110):
         expansion.append(1)
         clock -= 1
 print(expansion)
-print(2 + fractionalSum(4, expansion))
+print(2 + fractionalSum(3, expansion))
