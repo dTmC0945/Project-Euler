bac7c3f4641f22ce59cc0b01172ae9c27a8735a1
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-11-30 21:13:37 +0000

    Update Euler_040.py

--- Euler_040.py
+++ Euler_040.py
@@ -4,7 +4,7 @@ import numpy as np  # import for ceiling function
 import time  # import for time calculation
 
 start_time = time.time()  # start the clock
-
+# Average solve time (M1 Mac) - 0.05 sec
 
 # function calculating the champernowne digits.
 def ChampernowneDigits(n):
