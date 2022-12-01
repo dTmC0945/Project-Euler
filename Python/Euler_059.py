47cd8579050eb03e9ea2016bffdc98d159e24dc4
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-12-01 14:50:52 +0000

    Update Euler_059.py

--- Python/Euler_059.py
+++ Python/Euler_059.py
@@ -1,4 +1,3 @@
-
 def xorEncryption(bit_Value, bit_Key):
     while len(bit_Value) != len(bit_Key):
         if len(bit_Value) > len(bit_Key):
@@ -9,15 +8,32 @@ def xorEncryption(bit_Value, bit_Key):
     join_Digits = "".join(map(str, list(encryption)))
     return join_Digits, int(join_Digits, 2)
 
+
 # bit_Value, bit_Key = bin(value)[2:], bin(key)[2:]
 # open the textfile
 with open('Euler_059.txt') as file:
     lines = file.readlines()
 
 encrypted_List = list(map(int, lines[0].split(",")))
-print(encrypted_List)
+
+binary_array = []
+index = 0
+
+for e in encrypted_List:
+    binary_array.append(str(bin(e)[2:]))
+    while len(binary_array[index]) < 8:
+        binary_array[index] = "0" + binary_array[index]
+    index += 1
+
+print(binary_array)
+
 encryption_Key_Text = "the"
+
+binary_array = ''.join(binary_array)
 
 binary = ''.join(format(ord(x), 'b') for x in encryption_Key_Text)
 
-print(binary)
+repeat = print((len(binary_array)/len(binary)))
+
+
+#print(xorEncryption(binary_array[5], binary))
