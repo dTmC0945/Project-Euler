2967815689668df2ed3904688a5649fdfa199cd6
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-12-02 09:42:13 +0000

    Update Euler_041.py

--- Python/Euler_041.py
+++ Python/Euler_041.py
@@ -4,27 +4,51 @@ import time
 start_time = time.time()
 
 
-def primeFinder(n):
-    for div in range(2, int(np.sqrt(n)) + 1):
-        if n % div == 0 and n != div:
-            return 0
+# Sieve of Eratosthenes implementation
+def sievePrime(n):
+    array = np.arange(2, n)  # define the array of numbers up to the limit n
 
-    return n
+    temp_sieve = [2]  # declare a temporary sieve that retains the primes. Starts with 2.
 
-#def pandigital(n):
+    for value in range(2, int(np.ceil(np.sqrt(n)))):  # only care the numbers upto sqrt of n for speed purposes.
+        if temp_sieve[value - 2] != value:  # skips the value if the looping value divides itself (save around 1 sec)
+            continue
+        else:
+            for x in range(value, len(range(n)), value):  # loops the multiplies of the prime numbers to increase speed.
+                if array[x - 2] % value == 0 and array[x - 2] != value:  # check if number divides with a number.
+                    array[x - 2] = 0  # as it is not prime, change its values as 0
+                    temp_sieve = array
+                else:
+                    continue
 
-looper = 999999999
-
-while looper > 10**8:
-    intArray = [char for char in str(looper)]
-    if len(intArray) == len(set(intArray)) and primeFinder(looper) != 0:
-        print(looper)
-        looper -= 2
-        break
-    else:
-        looper -= 2
-        pass
+    return array  # returns the sieve as output.
 
 
+prime_list = sievePrime(9999999)
 
+prime_list = prime_list[prime_list != 0]
+
+print(prime_list)
+
+for prime in range(0, len(prime_list)):
+    intArray = [char for char in prime_list[prime]]
+    while len(intArray) == 4 or len(intArray) == 7:
+        if len(intArray) == len(set(intArray)):
+            print(intArray)
+
+
+
+# while looper > 10 ** 8:
+#     intArray = [char for char in str(looper)]
+#     if 0 in intArray:
+#         break
+#     else:
+#         if len(intArray) == len(set(intArray)) and primeFinder(looper) != 0:
+#             looper -= 2
+#             break
+#         else:
+#             looper -= 2
+#             pass
+#
+# print(looper)
 print("--- %s seconds ---" % (time.time() - start_time))
