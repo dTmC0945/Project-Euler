14d9d545ca31f26aba74aee4ef77bc5947c2c7bd
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-12-02 10:35:16 +0000

    Update Euler_041.py

--- Python/Euler_041.py
+++ Python/Euler_041.py
@@ -1,7 +1,12 @@
-import numpy as np
-import time
+# Project Euler Question 41 - Pan digital Prime
 
-start_time = time.time()
+import numpy as np  # numpy for array, sqrt and ceil
+import time  # import for time calculation
+
+start_time = time.time()  # start the clock
+
+
+# Average solve time (M1 Mac) - 5.6 sec
 
 
 # Sieve of Eratosthenes implementation
@@ -24,42 +29,49 @@ def sievePrime(n):
     return array  # returns the sieve as output.
 
 
-def filter(array):
+# Number filter to filter out numbers that are not 1 to n
+def filteredPrime(array):
+    # Sort out those pesky integers
     sorted_int = sorted(array)
 
-    for ind in range(0,len(sorted_int) - 1):
+    # Loop through each int list.
+    for ind in range(0, len(sorted_int) - 1):
+
+        # Check if each preceding number is 1 bigger than the other. If not return 0
         if sorted_int[ind + 1] - sorted_int[ind] != 1:
             return 0
 
-    return array
+    # As the question states 1 to n it must include 1.
+    if 1 not in sorted_int:
+        return 0
+    else:
+        return array # else return the array
 
 
+# we only need to check numbers that are 4 digits or 7 digits.
 prime_list = sievePrime(7654321)
 
+max_prime = 0
+# remove the zeroes
 prime_list = prime_list[prime_list != 0]
 
-print(prime_list)
-
+# main checking place ...
 for prime in range(0, len(prime_list)):
+
+    # turn the prime number into a list of string.
     intArray = list(str(prime_list[prime]))
+
+    # check if contains 4 digits or 7 digits
     if len(intArray) == 4 or len(intArray) == 7:
-        if "0" not in intArray and len(intArray) == len(set(intArray)):
-            print(filter(list(map(int, intArray))))
+
+        # make sure 0 is not in array and filteredPrime function does not return 0.
+        if "0" not in intArray and filteredPrime(list(map(int, intArray))) != 0:
+            max_prime = prime_list[prime]
 
     else:
         continue
 
-# while looper > 10 ** 8:
-#     intArray = [char for char in str(looper)]
-#     if 0 in intArray:
-#         break
-#     else:
-#         if len(intArray) == len(set(intArray)) and primeFinder(looper) != 0:
-#             looper -= 2
-#             break
-#         else:
-#             looper -= 2
-#             pass
-#
-# print(looper)
+print("The biggest pan digital number is: %s" % max_prime)
 print("--- %s seconds ---" % (time.time() - start_time))
+
+# End of code
