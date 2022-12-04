051d139d9526eefabb67f37ea2ce0e469f72b26b
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-12-04 11:40:21 +0000

    Update Euler_014 - Longest Collatz Sequence.py

---
+++ Python/Euler_014 - Longest Collatz Sequence.py
@@ -0,0 +1,33 @@
+# Project Euler Question 1 - Multiples of 3 or 5
+
+import time  # import for time calculation
+
+start_time = time.time()  # start the clock
+
+
+# Average solve time (M1 Mac) - 15.3 sec
+
+
+# The function that calculate the collatz chain
+def Collatz(n):
+    count = 1
+    while n > 1:
+        count += 1
+        if n % 2 == 0:
+            n /= 2
+        else:
+            n = 3 * n + 1
+    return count
+
+
+limit = 1000000  # limit given in the question
+maxValue, maxCount = 0, 0  # setting the parameters
+
+for number in range(1, limit):
+    if Collatz(number) > maxCount:
+        maxCount, maxValue = Collatz(number), number
+
+print("The longest chain under %s is given by number %s" % (limit, maxValue))
+print("--- %s seconds ---" % (time.time() - start_time))
+
+# End of code
