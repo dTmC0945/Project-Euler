6ffbf9d429e0b406c00465cde87627f2f1f07559
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-12-04 11:29:36 +0000

    Update

--- Python/Euler_011.py
+++ Python/Euler_011.py
@@ -1,7 +1,11 @@
+# Project Euler Question 11 - Largest product in a grid
+
 import numpy as np  # for arrays and stuff
 import time  # import for time calculation
 
 start_time = time.time()  # start the clock
+
+# Average solve time (M1 Mac) - 0.0012 sec
 
 # open the textfile
 with open('Text Files/Euler_011.txt') as file:
@@ -13,6 +17,7 @@ mappedLines = list(map(lambda s: s.strip(), lines))
 # convert the number list to integers and a 2D array
 index = 0
 array = np.zeros([20, 20])
+
 for text in mappedLines:
     array[index] = list(map(int, text.split(" ")))
     index += 1
@@ -29,23 +34,24 @@ for a in range(0, 16):
 # 2nd case is left and right
 for a in range(0, 20):
     for b in range(0, 16):
-        leftright = array[a][b + 0] * array[a][b + 1] * array[a][b + 2] * array[a][b + 3]
-        if leftright > maximum:
-            maximum = leftright
+        left_right = array[a][b + 0] * array[a][b + 1] * array[a][b + 2] * array[a][b + 3]
+        if left_right > maximum:
+            maximum = left_right
 
 # 3rd case is left diagonal
 for a in range(0, 16):
     for b in range(0, 16):
-        leftdiag = array[a + 0][b + 0] * array[a + 1][b + 1] * array[a + 2][b + 2] * array[a + 3][b + 3]
-        if leftdiag > maximum:
-            maximum = leftdiag
+        left_diag = array[a + 0][b + 0] * array[a + 1][b + 1] * array[a + 2][b + 2] * array[a + 3][b + 3]
+        if left_diag > maximum:
+            maximum = left_diag
 
 # 4th case is right diagonal
 for a in range(0, 16):
     for b in range(0, 16):
-        rightdiag = array[a + 3][b + 0] * array[a + 2][b + 1] * array[a + 1][b + 2] * array[a + 0][b + 3]
-        if rightdiag > maximum:
-            maximum = rightdiag
+        right_diag = array[a + 3][b + 0] * array[a + 2][b + 1] * array[a + 1][b + 2] * array[a + 0][b + 3]
+        if right_diag > maximum:
+            maximum = right_diag
 
-print(int(maximum))
+# Print out the answer
+print("The greatest product of four adjacent numbers in the same direction is:", int(maximum))
 print("--- %s seconds ---" % (time.time() - start_time))
