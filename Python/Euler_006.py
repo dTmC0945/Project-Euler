69d871eab664cbfab7c8ac118c9a42149b777924
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-12-04 11:03:39 +0000

    Update Euler_006.py

--- Python/Euler_006.py
+++ Python/Euler_006.py
@@ -7,11 +7,11 @@ start_time = time.time()  # start the clock
 
 # Average solve time (M1 Mac) - 0.00006 sec
 
-
+# Calculate the sum of square number up to n
 def sumSquare(n):
     return sum(map(lambda x: pow(x, 2), range(n + 1)))
 
-
+# Calculate the square of the sum of numbers up to n
 def squareSum(n):
     return pow(sum(map(lambda x: x, range(n + 1))), 2)
 
