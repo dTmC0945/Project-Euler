748805dba403d4a2cff1d1578bca9a988cb18c6e
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-12-04 10:59:38 +0000

    Update Euler_008.py

--- Python/Euler_008.py
+++ Python/Euler_008.py
@@ -1,12 +1,12 @@
 # Project Euler Question 8 - Largest product in a series
 
-from functools import reduce # for the lambda product function
-import numpy as np #import of the zeros array
+from functools import reduce  # for the lambda product function
+import numpy as np  # import of the zeros array
 import time  # import for time calculation
 
 start_time = time.time()  # start the clock
 
-# Average solve time (M1 Mac) - 0.0001 sec
+# Average solve time (M1 Mac) - 0.006 sec
 
 # open the textfile
 with open('Text Files/Euler_008.txt') as file:
@@ -15,21 +15,23 @@ with open('Text Files/Euler_008.txt') as file:
 # remove the \n from each line
 mappedLines = list(map(lambda s: s.strip(), lines))
 
-p = []
+p = []  # create an empty array to put the list into a single array
 
+# put the values read to the array
 for ls in mappedLines:
     p.append(list(ls))
 
-collapse = sum(p, [])  # collapsing of matrix
+collapse = sum(p, [])  # collapsing of matrix from 2D to 1D
 
-number = 13
+number = 13  # number of digits given in the question
 
+# create an empty array for the window that traverses the list p.
 window = np.zeros(number)
-size = len(window)
 
-maxValue = 0
-for a in range(len(collapse) - size):
-    for b in range(size):
+maxValue = 0  # initial maximum value for the digit products
+
+for a in range(len(collapse) - number):
+    for b in range(number):
         window[b] = collapse[a + b]
 
     intWin = list(map(int, window))
@@ -39,8 +41,7 @@ for a in range(len(collapse) - size):
     if mult > maxValue:
         maxValue = mult
 
-
-print("The largest product of the %s digits is: %s" %(number, maxValue))
+print("The largest product of the %s digits is: %s" % (number, maxValue))
 print("--- %s seconds ---" % (time.time() - start_time))
 
 # End of code
