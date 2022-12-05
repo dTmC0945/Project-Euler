43cfe92c6a36f9b30da743e241273750b600ac4d
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-12-05 12:33:04 +0000

    Update Euler_013 - Large Sum.py

--- Python/Euler_013 - Large Sum.py
+++ Python/Euler_013 - Large Sum.py
@@ -1,3 +1,11 @@
+# Project Euler Question 13 - Large Sum
+
+import time  # import for time calculation
+
+start_time = time.time()  # start the clock
+
+# Average solve time (M1 Mac) - 0.0003 sec
+
 # open the textfile
 with open('Text Files/Euler_013.txt') as file:
     lines = file.readlines()
@@ -15,4 +23,9 @@ intLines = list(map(int, firstTen))
 output = str(sum([x for x in intLines]))
 
 # print out the ten digits
-tendigits = print(output[0:10])
+tendigits = output[0:10]
+
+print("The first ten digits of the sum is:", tendigits)
+print("--- %s seconds ---" % (time.time() - start_time))
+
+# End of code
