8c2dd80578a861b10e9d4a62a2350d7e5fedbef0
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-12-05 12:06:26 +0000

    Update Euler_080.py

--- Python/Euler_080.py
+++ Python/Euler_080.py
@@ -1,7 +1,17 @@
 from bigfloat import *
 from decimal import *
-getcontext().prec = 100  # Change the precision
+
+# Set the precision required by the question
+getcontext().prec = 150
 
 array = []
 
-array.append(sum(list(map(int, list(str(Decimal(2).sqrt()))[2:])))))
+for value in range(2, 101):
+    square_Root = list(str(Decimal(value).sqrt()))
+    if "." not in square_Root:
+        print(value)
+        continue
+    array.append(sum(list(map(int, (square_Root[square_Root.index(".") + 1:square_Root.index(".") + 101])))))
+
+
+print(sum(array))
