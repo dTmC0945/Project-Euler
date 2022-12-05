65edea1974ec92edd8ba578aefbe73d757cb0afb
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-12-05 20:41:13 +0000

    Update Euler_066.py

--- Python/Euler_066.py
+++ Python/Euler_066.py
@@ -1,38 +1,38 @@
 import numpy as np
 
+
 def perfectSquare(limit):
     array = []
     for value in range(2, int(np.ceil(np.sqrt(limit)))):
-        array.append(value**2)
+        array.append(value ** 2)
 
     return array
 
+
 p_squares = perfectSquare(1000)
 
-list = np.arange(2,1000)
+list = np.arange(2, 1000)
 
 filtered_list = set(list) - set(p_squares)
 
 max_x = 0
 max_Value = 0
-for val in filtered_list:
+for D in filtered_list:
     x, y = 1, 1
-    print(val)
     while True:
-        eq = x**2 - val * y**2
-        if x > 20000:
+        eq = x ** 2 - D * y ** 2
+        if x > 40000:
             break
         if eq > 1:
             y += 1
         elif eq < 1:
-            x +=1
+            x += 1
         elif eq == 1:
 
             break
 
     if x > max_x:
         max_x = x
-        max_Value = val
+        max_Value = D
 
-
-print(max_x)
+print(D)
