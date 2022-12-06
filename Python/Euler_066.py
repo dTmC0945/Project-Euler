a2d00b603a83570988f1b1468625f074e99329d2
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-12-06 08:44:07 +0000

    Update Euler_066.py

--- Python/Euler_066.py
+++ Python/Euler_066.py
@@ -14,25 +14,19 @@ p_squares = perfectSquare(1000)
 list = np.arange(2, 1000)
 
 filtered_list = set(list) - set(p_squares)
-filtered_list = [61]
 max_x = 0
 max_Value = 0
+y = 1
 for D in filtered_list:
-    x, y = 1, 1
+
+    eq = np.sqrt(1 + D * y ** 2)
     while True:
-        eq = x ** 2 - D * y ** 2
-        print(eq)
-
-        if eq > 1:
-            y += 1
-        elif eq < 1:
-            x += 1
-        elif eq == 1:
-
+        if eq.is_integer():
+            print(D, eq)
             break
 
-    if x > max_x:
-        max_x = x
+    if eq > max_x:
+        max_x = eq
         max_Value = D
 
 print(max_Value, max_x)
