deb4cf88acb8e6fe283cd81f8132ce416f114ec6
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-12-09 10:53:19 +0000

    Update Euler_053.py

--- Python/Euler_053.py
+++ Python/Euler_053.py
@@ -7,9 +7,27 @@ start_time = time.time()  # start the clock
 
 # Average solve time (M1 Mac) - 0.0001 sec
 
-# factorial function
+# Define the factorial function (tried avoiding recursion for speed purposes)
 def factorial(n):
-    if n == 1:
-        return 1
-    else:
-        return n * factorial(n - 1)
+    p = 1  # first factorial number (n0 = 1)
+    i = 2  # second factorial number (n1 = 1)
+    while i <= n:
+        p *= i  # while loop is true multiply the p with the previous value
+        i += 1  # increase the value
+    return p
+
+def binomial(n, r):
+    return factorial(n) / (factorial(r) * factorial(n -r))
+
+count = 0
+
+for high in range(2, 101):
+    for low in range(1, high):
+        value = binomial(high, low)
+        if value > 1000000:
+            count += 1
+
+print("There are")
+print("--- %s seconds ---" % (time.time() - start_time))
+
+# End of code
