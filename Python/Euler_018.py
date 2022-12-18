dc60a6ca6e53a7485688e2ca059d8bba7aff4c0e
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-12-18 10:58:24 +0100

    Update ...

--- Python/Euler_018.py
+++ Python/Euler_018.py
@@ -1,3 +1,8 @@
 # open the textfile
 with open('Text Files/Euler_018.txt') as file:
     lines = file.readlines()
+
+mappedLines = list(map(lambda s: s.strip(), lines))
+
+for text in mappedLines:
+    print(text.split())
